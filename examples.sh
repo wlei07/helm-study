91b49f64b7b813b053a29d3a386c877fecc54554
Lei Wang <lei.wang@zetes.com>
2025-05-21 07:51:10 +0200

    repo command

--- examples.sh
+++ examples.sh
@@ -1,10 +1,17 @@
 # goto https://artifacthub.io/ search chart, e.g. wordpress
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo list
 #based on information in index.yaml, it can update accordingly https://repo.broadcom.com/bitnami-files/index.yaml
 helm repo update
-helm search repo wordpress
+helm repo remove bitnami
 
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm search repo wordpress
+helm search repo wordpress --versions
 helm search repo prometheus --max-col-width 20
+
 helm show chart bitnami/wordpress
+helm show readme bitnami/wordpress
+helm show values bitnami/wordpress
+
+helm repo --help
