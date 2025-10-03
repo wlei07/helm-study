061215979f6d91de6d8a19d46f5401e3b77d21cc
Lei <mail.lei.wang@gmail.com>
2025-10-03 22:08:13 +0200

    in progress test hook extract and decouple test script

---
+++ test-hook/files/test-scripts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Installing curl..."
+apk update && api add --no-cache curl
+
+echo "Executing curl request to {{ .Release.Name }}"
+curl http://{{ include "test-hook.fullname" . }}:{{ .Values.service.port }}
