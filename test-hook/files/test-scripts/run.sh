8e61e9cba6b4cb31808eb56e683802440cf370e5
Lei <mail.lei.wang@gmail.com>
2025-10-06 00:01:10 +0200

    in progress, add configuration values to values.yaml

--- test-hook/files/test-scripts/run.sh
+++ test-hook/files/test-scripts/run.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-echo "Installing curl..."
-apk update && apk add --no-cache curl
+# echo "Installing curl..."
+# apk update && apk add --no-cache curl
 
-echo "Executing curl request to {{ .Release.Name }}"
-curl http://{{ include "test-hook.fullname" . }}:{{ .Values.service.port }}
+# echo "Executing curl request to {{ .Release.Name }}"
+# curl http://{{ include "test-hook.fullname" . }}:{{ .Values.service.port }}
+
+set -eu
+
+ping -c 4 google.com
+echo "Executing HTTP request to {{ .Release.Name }} (wget)"
+# print headers and fail on non-2xx:
+wget -S --spider "http://{{ include "test-hook.fullname" . }}:{{ .Values.service.port }}"
+# or to fetch body quietly:
+# wget -qO- "http://{{ include "test-hook.fullname" . }}:{{ .Values.service.port }}" >/dev/null
